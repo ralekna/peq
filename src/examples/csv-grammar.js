affad5ac92100b804c1545721535ce6c1eb29e88
Rytis Alekna <r.alekna@gmail.com>
2019-08-19 23:30:33 +0300

    Added CSV grammar example

---
+++ src/examples/csv-grammar.js
@@ -0,0 +1,45 @@
+/*
+   Copyright 2019 Rytis Alekna <r.alekna@gmail.com>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+/*
+ * CSV grammar example based on https://tools.ietf.org/html/rfc4180 specification
+ */
+const {grammar, one, oneOf, all, any, optional} = require('./../parser');
+
+const CRLF = one(`\r\n`, undefined, 'linebreak');
+const TextData = one(/^[^",\n\r]+/);
+const Quote = one(`"`);
+const DQuote = one(`""`, () => `"`);
+const Comma = one(`,`);
+const Escaped = all([
+  Quote,
+  {name: 'text', matcher: any(oneOf([TextData, CRLF, DQuote, Comma]), all => all.join(''))},
+  Quote
+], (all, {text}) => text);
+const Field = oneOf([TextData, Escaped]);
+const Record = all([
+  {name: 'head', matcher: Field},
+  {name: 'tail', matcher: any(all([Comma, Field], ([, field]) => field))}
+  ], (all, {head, tail}) => [head, ...tail]);
+const CsvDocument = all([
+  {name: 'head', matcher: Record},
+  {name: 'tail', matcher: any(all([CRLF, Record], ([, record]) => record))},
+  optional(CRLF)
+], (all, {head, tail}) => [head, ...tail]);
+
+module.exports = grammar(_ => [{
+  CsvDocument
+}, 'CsvDocument']);
---
+++ src/examples/csv-grammar.spec.js
@@ -0,0 +1,49 @@
+/*
+   Copyright 2019 Rytis Alekna <r.alekna@gmail.com>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+const {expect} = require('chai');
+const {describe, it} = require('mocha');
+const parser = require('./csv-grammar');
+
+describe('CSV grammar', () => {
+  it('should parse single comma separated line', () => {
+    expect(parser(`aaa,bbb,ccc`)).to.be.deep.equal([['aaa', 'bbb', 'ccc']]);
+  });
+
+  it('should parse single comma separated line with numbers and special chars', () => {
+    expect(parser(`111,@@@,...`)).to.be.deep.equal([['111', '@@@', '...']]);
+  });
+
+  it('should parse single comma separated line of single field with quotes', () => {
+    expect(parser(`"aaa"`)).to.be.deep.equal([['aaa']]);
+  });
+
+  it('should parse single comma separated line with quotes', () => {
+    expect(parser(`"aaa","bbb","ccc"`)).to.be.deep.equal([['aaa', 'bbb', 'ccc']]);
+  });
+
+  it('should parse multiple comma separated line', () => {
+    expect(parser(`aaa,bbb,ccc \r\nddd,eee,fff`)).to.be.deep.equal([['aaa', 'bbb', 'ccc '], ['ddd', 'eee', 'fff']]);
+  });
+
+  it('should parse single comma separated line with quotes and linebreaks inside', () => {
+    expect(parser(`"aaa","b \r\nbb","ccc"`)).to.be.deep.equal([['aaa', 'b \r\nbb', 'ccc']]);
+  });
+
+  it('should parse mixed line with quotes and linebreaks inside', () => {
+    expect(parser(`aaa,"b \r\nbb","ccc"\r\nddd,"e"",ee",fff`)).to.be.deep.equal([['aaa', 'b \r\nbb', 'ccc'], ['ddd', 'e",ee', 'fff']]);
+  });
+});
